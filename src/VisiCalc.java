2e08d1ebcaaeb7daf68b8dc89009ca4efacc6ba3
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-18 08:19:36 -0700

    Update VisiCalc.java

--- src/VisiCalc.java
+++ src/VisiCalc.java
@@ -2,6 +2,8 @@ public class VisiCalc {
 
 	public static void main(String[] args) {
 		// create an array of cells
+		Cells[0] = new CellString[12];
+		Cells[i] = new CellString("wow");
 
 		
 		// initialize the array
