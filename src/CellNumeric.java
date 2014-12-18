68a4383025d868ea501c133b6fc06b7a18373118
mama-egg <Kinseygoesrawr@gmail.com>
2014-12-18 08:19:41 -0700

    Update CellNumeric.java

--- src/CellNumeric.java
+++ src/CellNumeric.java
@@ -1,1 +1,13 @@
-// placeholder
+public class VisiCalc extends Cell {
+  
+  super(value);
+  
+  public VisiCalc v1= new VisiCalc() {
+    
+  }
+  
+  public VisiCalc v2= new VisiCalc() {
+    VisiCalc.setValue(value);
+  }
+  
+}
